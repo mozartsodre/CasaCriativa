efcd3f9f5066fe5c3cd7e71f434499e83da1b9a8
mozartsodre <mozart.sodre@al.resilia.com.br>
2020-03-29 19:57:37 -0300

    Aula 5 finalizada
    Agora sim o projeto foi concluído e todas as partes estão conversando. Bando de dados recebe os dados inseridos nos campos e alimentam todas as tabelas que retornam dinâmicamente os cards das páginas. Ô felicidade em finalizar esse projeto!

--- server.js
+++ server.js
@@ -8,39 +8,13 @@
 const express = require("express");
 const server = express();
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste expedita, illo officiis laboriosam in consectetur! Delectus voluptatibus facere quasi culpa nihil nemo. Debitis nisi, corporis placeat tempora alias vitae tenetur?",
-        url: "http://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste expedita, illo officiis laboriosam in consectetur! Delectus voluptatibus facere quasi culpa nihil nemo. Debitis nisi, corporis placeat tempora alias vitae tenetur?",
-        url: "http://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste expedita, illo officiis laboriosam in consectetur! Delectus voluptatibus facere quasi culpa nihil nemo. Debitis nisi, corporis placeat tempora alias vitae tenetur?",
-        url: "http://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em família",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste expedita, illo officiis laboriosam in consectetur! Delectus voluptatibus facere quasi culpa nihil nemo. Debitis nisi, corporis placeat tempora alias vitae tenetur?",
-        url: "http://rocketseat.com.br"
-    },
-]
+const db = require("./db")
 
 // Configurar arquivos estáticos (CSS, Scripts, Imagens)
 server.use(express.static("public"))
+
+// Habilitar uso do req.body
+server.use(express.urlencoded ({extended:true}))
 
 // Configuração do nunjucks
 const nunjucks = require("nunjucks")
@@ -55,7 +29,13 @@ nunjucks.configure("views", {
 
 server.get("/", function(require,response) {
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err,rows){
+        if(err) {
+            console.log(err)
+            return response.send("Erro no banco de dados!")
+        }
+
+    const reversedIdeas = [...rows].reverse()
 
     let lastIdeas = []
     for (let idea of reversedIdeas){
@@ -65,13 +45,55 @@ server.get("/", function(require,response) {
     }
 
     return response.render("index.html",{ideas:lastIdeas})
+    })
+
 });
 
 server.get("/ideias", function(require,response) {
 
-    const reversedIdeas = [...ideas].reverse()
+    require.query
 
-    return response.render("ideias.html", {ideas: reversedIdeas})
+    db.all(`SELECT * FROM ideas`, function(err,rows){
+        if(err) {
+            console.log(err)
+            return response.send("Erro no banco de dados!")
+        }
+
+        const reversedIdeas = [...rows].reverse()
+
+        return response.render("ideias.html", {ideas: reversedIdeas})
+    })
+
 });
+
+server.post("/", function(req, res){
+    // Inserir dados na tabela
+        const query = `
+            INSERT INTO ideas(
+                image,
+                title,
+                category,
+                description,
+                link
+        ) VALUES (?,?,?,?,?);
+        `
+
+        const values = [
+            req.body.image,
+            req.body.title,
+            req.body.category,
+            req.body.description,
+            req.body.link
+        ]
+
+        db.run(query, values, function(err){
+            if(err) {
+                console.log(err)
+                return response.send("Erro no banco de dados!")
+            }
+            return res.redirect("/ideias")
+        })
+})
+
 // Liguei meu servidor na porta 3000
 server.listen(3000);
